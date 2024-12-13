52b7bee5f93fafed4bb142f72f806545c0863f8e
Gabriel Tavares <gabrielc.tavares02@gmail.com>
2024-12-12 22:20:05 -0300

    All benchmarks modified successfully

--- benchmarks/gsm/gsm_add.h
+++ benchmarks/gsm/gsm_add.h
@@ -3,9 +3,6 @@
 
 #include "gsm_private.h"
 
-#define saturate(x) \
-    ((x) < MIN_WORD ? MIN_WORD : (x) > MAX_WORD ? MAX_WORD : (x))
-
 word gsm_add(word a, word b);
 word gsm_mult(word a, word b);
 word gsm_mult_r(word a, word b);
