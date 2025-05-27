Fixed min-max scaling for fine-tuning

gnn/analysis.py
@@ -1,5 +1,4 @@
-import copy
-from typing import List, Optional, Tuple, Union
+from typing import List, Optional, Union
 
 import numpy as np
 import pandas as pd
@@ -8,7 +7,7 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import r2_score
 from torch import Tensor
 
-from gnn.utils import mape
+from gnn.utils import percentage_diff
 
 
 def plot_learning_curves(
@@ -17,8 +16,8 @@ def plot_learning_curves(
     output_path: str,
     best_epoch: Optional[int] = None
 ):
-    n_epochs = len(train_errors)
-    if n_epochs != len(test_errors):
+    num_epochs = len(train_errors)
+    if num_epochs != len(test_errors):
         raise ValueError("Mismatch in number of training and test epochs")
     
     train_errors = list(map(lambda x: x / 100, train_errors))
@@ -28,18 +27,21 @@ def plot_learning_curves(
     sns.set_style("whitegrid")
 
     df = pd.DataFrame({
-        'Epoch': list(range(n_epochs)) * 2,
-        'Loss': train_errors + test_errors,
-        'Type': ['Train'] * n_epochs + ['Test'] * n_epochs
+        'Epoch': list(range(num_epochs)) * 2,
+        'Error': train_errors + test_errors,
+        'Type': ['Train'] * num_epochs + ['Test'] * num_epochs
     })
     ax = sns.lineplot(
-        x='Epoch', y='Loss', hue='Type', data=df, 
+        x='Epoch', y='Error', hue='Type', data=df, 
         palette={'Train': 'green', 'Test': 'red'}, 
         linewidth=2.5, marker='o'
     )
-    plt.title(f'Training Progress (Final Test Loss: {test_errors[-1]:.4f})', fontsize=14)
+    plt.title(
+        f'Training Progress (Final Test Error: {test_errors[-1]:.4f})', 
+        fontsize=14
+    )
     plt.xlabel('Epoch', fontsize=12)
-    plt.ylabel('MRE', fontsize=12)
+    plt.ylabel('Mean Relative Error', fontsize=12)
     plt.legend()
 
     if best_epoch is not None:
@@ -61,7 +63,7 @@ def plot_prediction_scatter(
     preds: List[float], 
     output_path: str,
     errors: Optional[Union[Tensor, List[float]]] = None,
-    mre: Optional[float] = None
+    mean_error: Optional[float] = None
 ):
     max_val = max(max(targets), max(preds)) * 1.1
     min_val = min(min(targets), min(preds)) * 0.9
@@ -76,19 +78,24 @@ def plot_prediction_scatter(
     )
     # Regression line
     sns.regplot(
-        x=targets, y=preds, scatter=False, color='blue', label='Trend Line'
+        x=targets, y=preds, scatter=False, color='blue', 
+        label='Trend Line'
     )
     # Scatter plot
-    sns.scatterplot(x=targets, y=preds, alpha=0.6, edgecolor='w', label='Predictions')
+    sns.scatterplot(
+        x=targets, y=preds, alpha=0.6, edgecolor='w', 
+        label='Predictions'
+    )
 
-    if mre is None:
+    if mean_error is None:
         if errors is None:
-            errors = mape(preds, targets)
-        mre = np.mean(errors)
+            errors = percentage_diff(preds, targets)
+        mean_error = np.mean(errors)
             
     r2 = r2_score(targets, preds)
     plt.text(
-        min_val*1.05, max_val*0.9, f'Mean Relative Error: {mre:.2f}\nR²: {r2:.2f}', 
+        min_val*1.05, max_val*0.9, 
+        f'Mean Relative Error: {mean_error:.2f}\nR²: {r2:.2f}', 
         bbox=dict(facecolor='white', alpha=0.8)
     )
     
@@ -103,20 +110,25 @@ def plot_prediction_scatter(
 
 
 def plot_prediction_bars(
-    test_results: List[List[Tuple[float, float]]],
-    solution_indices: List[int],
+    targets: List[float],
+    preds: List[float],
+    indices: List[int],
     benchmark: str, 
     metric: str, 
     output_path: str,
-    epoch: int = -1,
-    mre: Optional[float] = None
+    errors: Optional[Union[Tensor, List[float]]] = None,
+    mean_error: Optional[float] = None
 ):
     """Plot per-benchmark instance-level predictions and errors"""
-    targets = [r[1] for r in test_results[epoch]]
-    preds = [r[0] for r in test_results[epoch]]
-    errors = [mape(p, t).item() for p, t in zip(preds, targets)]
-    indices = copy.deepcopy(solution_indices)
-    x = list(range(len(indices)))
+    num_instances = len(targets)
+    if num_instances != len(preds) or num_instances != len(indices):
+        raise ValueError("Mismatch in number of targets, predictions and indices")
+    
+    if errors is None:
+        errors = [percentage_diff(p, t).item() for p, t in zip(preds, targets)]
+
+    if mean_error is None:
+        mean_error = np.mean(errors)
 
     df = pd.DataFrame({
         'index': indices,
@@ -129,6 +141,7 @@ def plot_prediction_bars(
     _, ax = plt.subplots(figsize=(16, 6), dpi=180)
     sns.set_style("whitegrid")
 
+    x = np.arange(num_instances)
     ax.bar(x, df['target'], color='blue', alpha=0.5, label='Targets')
     ax.bar(x, df['prediction'], color='darkorange', alpha=0.5, label='Predictions')
 
@@ -148,15 +161,14 @@ def plot_prediction_bars(
     ax.set_xticklabels(
         df['index'], rotation=90, ha='center', va='top', fontsize=4, alpha=0.9
     )
-    if mre is None:
-        mre = np.mean(df['error'])
+
     ax.text(
-        0.12, 0.95, f"Mean Relative Error: {mre:.2f}%", 
+        0.12, 0.95, f"Mean Relative Error: {mean_error:.2f}%", 
         transform=ax.transAxes, fontsize=11, ha='center'
     )
-    ax.set_title(f"Predictions for {benchmark.upper()} ({metric.title()})", fontsize=14)
+    ax.set_title(f"Predictions for {benchmark.upper()} ({metric.upper()})", fontsize=14)
     ax.set_xlabel('Instance Index', fontsize=12)
-    ax.set_ylabel(f'{metric.title()}', fontsize=12)
+    ax.set_ylabel(f'{metric.upper()}', fontsize=12)
     ax.legend()
 
     plt.tight_layout()

gnn/fine_tuning/fine_tuning.py
@@ -70,7 +70,6 @@ def load_model(
     model_args['dropout'] = dropout
     model = HGT(**model_args)
     model.load_state_dict(torch.load(model_path, map_location=DEVICE))
-
     return model.to(DEVICE)
 
 
@@ -83,7 +82,7 @@ def main(args: Dict[str, str]):
     feature_ranges_path = args.get("feature_ranges", "")
     target_metric = args.get("target", "snru")
     epochs = int(args.get("epochs", 10))
-    lr = float(args.get("learning_rate", 1e-5))
+    lr = float(args.get("learning_rate", 1e-4))
     dropout = float(args.get("dropout", 0.3))
     output_dir = args.get("output_dir", "")
 
@@ -117,7 +116,7 @@ def main(args: Dict[str, str]):
             pretraining_args = pickle.load(f)
 
         betas = pretraining_args.get('betas', (0.9, 0.999))
-        weight_decay = pretraining_args.get('weight_decay', 5e-4)
+        weight_decay = pretraining_args.get('weight_decay', 1e-4)
     
         loss = pretraining_args.get('loss', 'mse')
         if loss == 'mse':
@@ -133,20 +132,15 @@ def main(args: Dict[str, str]):
         max_norm = pretraining_args.get('max_norm', 5.0)
     else:
         betas = (0.9, 0.999)
-        weight_decay = 5e-4
+        weight_decay = 1e-4
         loss_fn = nn.MSELoss()
         max_norm = 5.0
 
-    # optimizer = torch.optim.AdamW(
-    #     model.parameters(), 
-    #     lr=lr,
-    #     betas=betas,
-    #     weight_decay=weight_decay
-    # )
-    optimizer = torch.optim.Adam(
+    optimizer = torch.optim.AdamW(
         model.parameters(), 
         lr=lr,
-        betas=betas
+        betas=betas,
+        weight_decay=weight_decay
     )
     
     # Fine-tune the model

gnn/train.py
@@ -3,7 +3,6 @@ import argparse
 import random
 import copy
 import pickle
-from collections import defaultdict
 from typing import List, Dict, Tuple, Optional, Union
 
 import matplotlib
@@ -22,7 +21,7 @@ from gnn.data.graph import (
     NODE_FEATURE_DIMS
 )
 from gnn.data.dataset import HLSDataset
-from gnn.utils import mape
+from gnn.utils import percentage_diff
 from gnn.analysis import (
     plot_prediction_bars,
     plot_prediction_scatter,
@@ -41,16 +40,20 @@ def static_vars(**kwargs):
     return decorate
 
 
-@static_vars(min_mre=1e10, best_epoch=0, best_model=None)
+@static_vars(
+    min_mre=1e10, 
+    best_epoch=0, 
+    best_preds=None,
+    best_model=None
+)
 def evaluate(
     model: nn.Module,
     loader: DataLoader,
     epoch: int,
     verbosity: int = 1,
-    output_dir: Optional[str] = None,
     mode: str = 'test',
     exp_adjust: bool = False
-) -> List[Tuple[float, float]]:
+) -> float:
     if verbosity > 1:
         print(f"\nEvaluating on {mode} set\n")
 
@@ -63,35 +66,25 @@ def evaluate(
         preds.append(pred.item())
         targets.append(batch.y.item())
         
-    if output_dir:
-        with open(f"{output_dir}/{mode}.log", 'a') as f:
-            for p, t in zip(preds, targets):
-                f.write(f"{epoch},{t},{p},{t-p}\n")
-
     if exp_adjust:
         preds, targets = np.expm1(preds), np.expm1(targets)
 
-    mre = mape(
-        torch.tensor(preds), 
-        torch.tensor(targets)
-    ).mean().item()
-
+    mre = percentage_diff(preds, targets).mean().item()
     if mre < evaluate.min_mre:
         evaluate.min_mre = mre
         evaluate.best_epoch = epoch
+        evaluate.best_preds = preds
         evaluate.best_model = copy.deepcopy(model.state_dict())
-
-    results = list(zip(preds, targets))
-
-    if verbosity > 0:
-        for p, t in results:
-            print(f"Target: {t}; Prediction: {p}")
             
+    if verbosity > 0:
+        for p, t in zip(preds, targets):
+             print(f"Target: {t}; Prediction: {p}")
+
         print(f"\nMRE on {mode} set: {mre:.2f}%")
         print(f"Best MRE so far: {evaluate.min_mre:.2f}%"
               f" at epoch {evaluate.best_epoch + 1}\n")
     
-    return results
+    return mre
 
 
 def train_model(
@@ -103,17 +96,16 @@ def train_model(
     epochs: int,
     scheduler: Optional[LRScheduler] = None,
     verbosity: int = 1,
-    output_dir: Optional[str] = None,
     max_norm: Optional[float] = None,
     exp_adjust: bool = False,
-) -> Dict[str, List[List[Tuple[float, float]]]]:
-    results = defaultdict(list)
+) -> Tuple[List[float], List[float]]:
+    train_mres, test_mres = [], []
 
     for epoch in range(epochs):
         if verbosity > 0:
             print(f"Epoch {epoch + 1}/{epochs}\n")
 
-        preds, targets = [], []
+        train_errors = []
                 
         model.train()
         for batch in train_loader:
@@ -130,40 +122,31 @@ def train_model(
                 clip_grad_norm_(model.parameters(), max_norm=max_norm)
 
             optimizer.step()
-
             if scheduler is not None:
                 scheduler.step()
 
-            pred, target = pred.tolist(), target.tolist()
-            preds.extend(pred)
-            targets.extend(target)
+            if exp_adjust:
+                pred, target = np.expm1(pred), np.expm1(target)
+
+            train_errors.append(percentage_diff(pred, target))
+
             if verbosity > 1:
-                if exp_adjust:
-                    pred, target = np.expm1(pred), np.expm1(target)
-                for p, t in zip(pred, target):
+                for p, t in zip(pred.tolist(), target.tolist()):
                     print(f"Target: {t}; Prediction: {p}")
 
-        if output_dir:
-            with open(f"{output_dir}/train.log", 'a') as f:
-                for p, t in zip(preds, targets):
-                    f.write(f"{epoch},{t},{p},{t-p}\n")
-
-        if exp_adjust:
-            preds, targets = np.expm1(preds), np.expm1(targets)
-
-        results['train'].append(list(zip(preds, targets)))
+        train_mre = torch.stack(train_errors).mean().item()
+        train_mres.append(train_mre)
 
         model.eval()
         with torch.no_grad():
-            test_results = evaluate(
+            test_mre = evaluate(
                 model, test_loader, epoch,
                 verbosity=verbosity, 
-                output_dir=output_dir, 
                 exp_adjust=exp_adjust
             )
-            results['test'].append(test_results)
+            test_mres.append(test_mre)
 
-    return results
+    return train_mres, test_mres
 
 
 def main(args: Dict[str, str]):
@@ -204,7 +187,7 @@ def main(args: Dict[str, str]):
     benches = sorted(os.listdir(full_dataset_dir))
     train_benches = [b for b in benches if b != test_bench]
 
-    model_dir = f"{output_dir}/models/{target_metric}_{test_bench}"
+    model_dir = f"{output_dir}/models/{target_metric.upper()}_{test_bench}"
     pretrained_dir = f"{model_dir}/pretrained"
     model_info_dir = f"{model_dir}/model_info"
     training_info_dir = f"{model_dir}/training_info"
@@ -231,7 +214,7 @@ def main(args: Dict[str, str]):
     }
     model = HGT(**model_args).to(DEVICE)
 
-    model_args_path = f"{model_info_dir}/{target_metric}_estimator_args.pkl"
+    model_args_path = f"{model_info_dir}/{target_metric.upper()}_estimator_args.pkl"
     with open(model_args_path, 'wb') as f:
         pickle.dump(model_args, f)
 
@@ -244,16 +227,11 @@ def main(args: Dict[str, str]):
     else:
         raise ValueError(f"Unsupported loss function: {loss}")
 
-    # optimizer = torch.optim.AdamW(
-    #     model.parameters(), 
-    #     lr=learning_rate, 
-    #     betas=betas, 
-    #     weight_decay=weight_decay
-    # )
-    optimizer = torch.optim.Adam(
-        model.parameters(),
-        lr=learning_rate,
-        betas=betas
+    optimizer = torch.optim.AdamW(
+        model.parameters(), 
+        lr=learning_rate, 
+        betas=betas, 
+        weight_decay=weight_decay
     )
 
     train_loader, test_loader, feat_ranges = prepare_data_loaders(
@@ -262,8 +240,7 @@ def main(args: Dict[str, str]):
         test_benches=test_bench, 
         train_benches=train_benches, 
         batch_size=batch_size, 
-        log_scale=log_scale,
-        return_feature_ranges=True
+        log_scale=log_scale
     )
 
     with open(f"{model_info_dir}/feature_ranges.pkl", 'wb') as f:
@@ -278,46 +255,52 @@ def main(args: Dict[str, str]):
         anneal_strategy=anneal_strategy
     )
 
-    results = train_model(
+    train_errors, test_errors = train_model(
         model, loss_fn, optimizer, 
         train_loader, test_loader, epochs, 
         scheduler=scheduler, 
         verbosity=verbosity, 
-        output_dir=training_info_dir,
         max_norm=max_norm,
         exp_adjust=log_scale
     )
 
-    model_path = f"{pretrained_dir}/{target_metric}_estimator_{test_bench}.pt"
+    model_path = f"{pretrained_dir}/{target_metric.upper()}_estimator.pt"
     torch.save(obj=evaluate.best_model, f=model_path)
 
     indices = [data.solution_index for data in test_loader.dataset]
+    targets = [data.y.item() for data in test_loader.dataset]
+    preds = evaluate.best_preds
+
+    with open(f"{model_info_dir}/predictions.csv", 'w') as f:
+        f.write("Index,Target,Prediction\n")
+        for idx, target, pred in zip(indices, targets, preds):
+            f.write(f"{idx},{target},{pred}\n")
+
     plot_prediction_bars(
-        test_results=results["test"], 
-        solution_indices=indices, 
+        targets=targets,
+        preds=preds,
+        indices=indices, 
         benchmark=test_bench, 
         metric=target_metric, 
         output_path=f"{graphs_dir}/predictions.png", 
-        epoch=evaluate.best_epoch, 
-        mre=evaluate.min_mre
+        mean_error=evaluate.min_mre
     )
-    save_training_artifacts(results, graphs_dir)
+    save_training_artifacts(
+        test_targets=targets,
+        test_preds=preds,
+        test_errors=test_errors,
+        train_errors=train_errors,
+        output_dir=graphs_dir
+    )
 
 
 def save_training_artifacts(
-    results: Dict[str, List[List[Tuple[float, float]]]],
+    test_targets: List[float],
+    test_preds: List[float],
+    test_errors: List[float],
+    train_errors: List[float],
     output_dir: str
 ):
-    train_errors, test_errors = [], []
-    for train_results, test_results in zip(results['train'], results['test']):
-        train_errors.append(np.mean([mape(p, t) for p, t in train_results]))
-        test_errors.append(np.mean([mape(p, t) for p, t in test_results]))
-
-    best_test_results = results['test'][evaluate.best_epoch]
-    targets = [r[1] for r in best_test_results]
-    preds = [r[0] for r in best_test_results]
-    errors = [mape(p, t).item() for p, t in zip(preds, targets)]
-
     learning_curve_path = f"{output_dir}/learning_curve.png"
     scatter_path = f"{output_dir}/predictions_scatter.png"
 
@@ -328,10 +311,10 @@ def save_training_artifacts(
         best_epoch=evaluate.best_epoch
     )
     plot_prediction_scatter(
-        targets=targets, 
-        preds=preds, 
+        targets=test_targets, 
+        preds=test_preds, 
         output_path=scatter_path, 
-        errors=errors
+        mean_error=evaluate.min_mre
     )
     
 
@@ -349,9 +332,8 @@ def prepare_data_loaders(
     test_benches: Union[List[str], str],
     train_benches: Union[List[str], str],
     batch_size: int = 32,
-    log_scale: bool = False,
-    return_feature_ranges: bool = False
-) -> Tuple[DataLoader, DataLoader]:
+    log_scale: bool = False
+) -> Tuple[DataLoader, DataLoader, Dict[str, Tuple[float, float]]]:
     train_dataset = HLSDataset(
         root=dataset_dir, 
         metric=target_metric, 
@@ -369,12 +351,15 @@ def prepare_data_loaders(
         benchmarks=test_benches,
         log_scale=log_scale
     )
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
-    if return_feature_ranges:
-        return train_loader, test_loader, train_dataset.feature_ranges
-    else:
-        return train_loader, test_loader
+    train_loader = DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True
+    )
+    test_loader = DataLoader(
+        test_dataset, batch_size=1, shuffle=False
+    )
+    return (train_loader, test_loader, 
+            train_dataset.feature_ranges)
+
 
 
 def parse_arguments():
@@ -403,7 +388,7 @@ def parse_arguments():
                         help='The percentage of the cycle to increase the learning rate (default: 0.35).')
     parser.add_argument('-as', '--anneal-strategy', type=str, default='cos', choices=['cos', 'linear'], 
                         help='The annealing strategy for the learning rate scheduler (default: cos).')
-    parser.add_argument('-lt', '--log-scale', action='store_true',
+    parser.add_argument('-ls', '--log-scale', action='store_true',
                         help='Apply log transformation to the target variable.')
     parser.add_argument('-t', '--target', type=str, default='snru', choices=['snru', 'time', 'power', 'lut', 'ff', 'dsp', 'bram'],
                         help='The target metric to predict (default: snru).')

gnn/utils.py
@@ -1,7 +1,7 @@
 import torch
 
 
-def mape(pred, target):
+def percentage_diff(pred, target):
     pred, target = map(torch.as_tensor, (pred, target))
     avg = (torch.abs(pred) + torch.abs(target)) / 2
     return torch.where(